ee38d306b293e6625a5d15bf21d9d693121126f0
Shenangoat <77092662+Aleksandis@users.noreply.github.com>
2022-02-03 13:17:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import turtle
+ 
+# turtle object
+pen = turtle.Turtle()
+ 
+# draw face
+pen.fillcolor('yellow')
+pen.begin_fill()
+pen.circle(100)
+pen.end_fill()
+pen.up()
+
+import turtle
+ 
+# turtle object
+pen = turtle.Turtle()
+ 
+# function for creation of eye
+def eye(col, rad):
+    pen.down()
+    pen.fillcolor(col)
+    pen.begin_fill()
+    pen.circle(rad)
+    pen.end_fill()
+    pen.up()
+ 
+ 
+# draw face
+pen.fillcolor('yellow')
+pen.begin_fill()
+pen.circle(100)
+pen.end_fill()
+pen.up()
+ 
+# draw eyes
+pen.goto(-40, 120)
+eye('black', 15)
+
+pen.goto( 30, 120)
+eye('black', 15)
+
+pen.goto(-40, 85)
+pen.down()
+pen.right(90)
+pen.circle(40, 180)
+pen.up()
+
